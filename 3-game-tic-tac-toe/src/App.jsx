355a5118fdecb202c8715278c7cec33360ba662b
yosiaevan <yosiaevan23@gmail.com>
2025-03-18 13:07:22 +0700

    menambahkan styling

--- 3-game-tic-tac-toe/src/App.jsx
+++ 3-game-tic-tac-toe/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
-function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+function Square({ value, onSquareClick, className }) {
+  return <button className={`square ${className || ''}`} onClick={onSquareClick}>{value}</button>;
 }
 
 function Board({xIsNext, squares, onPlay}) {
@@ -29,15 +29,15 @@ function Board({xIsNext, squares, onPlay}) {
       <div className="status">{status}</div>
 
       <div className="board">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} className="rounded-top-left" />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} className="rounded-top-right" />
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} className="rounded-bottom-left" />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} className="rounded-bottom-right" />
       </div>
     </>
   )
